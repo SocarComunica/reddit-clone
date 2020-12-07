2eaa633a23c660fcc7a92cf5a42ceb98a6cadc3b
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2020-12-07 15:37:07 -0500

    Login resolver + id graphql types fixed

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "../types";
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from 'argon2';
 
 
@@ -13,13 +13,51 @@ class UsernamePasswordInput {
     password: string;
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[];
+
+    @Field(() => User, {nullable: true})
+    user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ): Promise<User> {
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2)
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: 'length must be greater than 2'
+                    }
+                ]
+            };
+        
+        if (options.password.length <= 8)
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: 'length must be greater than 8'
+                    }
+                ]
+            };
+        
         // Hashing password
         const hashedPassword = await argon2.hash(options.password);
         
@@ -28,25 +66,73 @@ export class UserResolver {
             username: options.username,
             password: hashedPassword
         });
-        await em.persistAndFlush(user);
+        try {
+            await em.persistAndFlush(user);
+        } catch (error) {
+            switch (error.code) {
+                case '23505':
+                    return {
+                        errors: [
+                            {
+                                field: 'username',
+                                message: 'username is already taken'
+                            }
+                        ]
+                    }
+                default:
+                    return {
+                        errors: [
+                            {
+                                field: 'default',
+                                message: 'please contact support'
+                            }
+                    ]
+                }
+            }
+        }
 
-        return user;
+        return { user };
     }
 
-    @Query(() => User, {nullable: true})
+    @Query(() => UserResponse)
     async login(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ): Promise<User | null> {
-        // Verifying user exists
+    ): Promise<UserResponse> {
+        // Verifying user exist
         const user = await em.findOne(User, { username: options.username });
         if (!user)
-            return null;
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: 'username does not exist'
+                    }
+                ]
+            };
+
+        // Validate password
+        if (options.password === '' || options.password === 'undefined')
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: 'password can not be empty'
+                    }
+                ]
+            };  
         
         // Checking password
-        if (await argon2.verify(user.password, options.password))
-            return user;
-        
-        return null;
+        if (!await argon2.verify(user.password, options.password))
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: 'password do not match'
+                    }
+                ]
+            };
+
+        return { user };
     }
 }
